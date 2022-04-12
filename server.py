7415ac7e3a68bb55e049c94b28e10368beaf35b5
joshmmiller2 <98432070+joshmmiller2@users.noreply.github.com>
2022-04-12 03:35:05 -0600

    commit

--- server.py
+++ server.py
@@ -1,25 +1,7 @@
-from flask import Flask, render_template, session, redirect,request
-
-app = Flask(__name__)
-
-app.secret_key="magical moonrock"
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+from flask_app import app
+from flask_app.controllers import surveys
 
 
-@app.route('/process',methods=['POST'])
-def process():
-    session['name'] = request.form['name']
-    session['location'] = request.form['location']
-    session['language'] = request.form['language']
-    session['comments'] = request.form['comments']
-    return redirect('/success')
 
-@app.route('/results')
-def success():
-    return render_template('results.html')
-    
 if __name__=="__main__":
     app.run(debug=True)
